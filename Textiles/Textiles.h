960fa2ebfdcead624300f1b88eb7dd552e4b8ec2
Jacob Morris <jacob.s.morris@gmail.com>
2018-03-11 14:26:01 -0600

    Initial commit
    - Setup Quick & Nimble via Carthage
    - Basic .gitignore file from github/gitignore/Swift

---
+++ Textiles/Textiles.h
@@ -0,0 +1,19 @@
+//
+//  Textiles.h
+//  Textiles
+//
+//  Created by Jacob Morris on 3/11/18.
+//  Copyright © 2018 Jacob Morris. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Textiles.
+FOUNDATION_EXPORT double TextilesVersionNumber;
+
+//! Project version string for Textiles.
+FOUNDATION_EXPORT const unsigned char TextilesVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Textiles/PublicHeader.h>
+
+
---
+++ TextilesTests/TextilesTests.swift
@@ -0,0 +1,19 @@
+//
+//  TextilesTests.swift
+//  TextilesTests
+//
+//  Created by Jacob Morris on 3/11/18.
+//  Copyright © 2018 Jacob Morris. All rights reserved.
+//
+
+import XCTest
+import Quick
+import Nimble
+
+class TextilesSpec : QuickSpec {
+  override func spec() {
+    it("is set up") {
+      expect(1).to(equal(1))
+    }
+  }
+}
